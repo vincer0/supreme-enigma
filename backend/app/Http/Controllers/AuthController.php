bf0fb6b7c01bebc64475c178a7e8bc6e9ebbb1e6
adam <pasierb.adam@gmail.com>
2026-01-03 14:07:00 +0100

    install cs-fixer

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
@@ -9,9 +11,14 @@ class AuthController extends Controller
     public function login(LoginRequest $request)
     {
         $data = $request->validated();
-       
+
         $email = $data['email'];
         $password = $data['password'];
+
+        $user = auth()->attempt([
+            'email' => $email,
+            'password' => $password,
+        ]);
     }
 
     public function register()
